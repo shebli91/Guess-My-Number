e5aed4811f0358952aa4cf17967f36f432e20c10
shebli91 <mhamadshebli@gmail.com>
2022-12-20 09:46:08 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const scoreText = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // when no input
+  if (!guess) {
+    displayMessage('⛔ No Number');
+
+    //when player Wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 correct Number !');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    // when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High' : '📉 Too Low');
+      score--;
+      scoreText(score);
+    } else {
+      displayMessage('💥 You Lost The Game ');
+      scoreText(0);
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  scoreText(score);
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
